cd58e6e8da53be48abb8b105ed648ee636e16ce4
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-11-10 01:15:16 -0500

    Added working hardware test

--- address_table_hardware_test.vhd
+++ address_table_hardware_test.vhd
@@ -19,12 +19,11 @@ end address_table_hardware_test;
 architecture address_table_hardware_test_rtl of address_table_hardware_test is
 
 	type test_state_type is
-		(base_state,
-			push_pin_state,
-			assert_trigger_state,
-			deassert_trigger_state,
-			latch_state);
+		(reset_state,
+			seen_state,
+			final_state);
 	signal test_state: test_state_type;
+	signal test_state_next: test_state_type;
 
 	signal internal_trigger: std_logic;
 	signal internal_output_ready: std_logic;
@@ -51,7 +50,7 @@ architecture address_table_hardware_test_rtl of address_table_hardware_test is
 	begin 
 		address_table_inst: address_table port map(
 			clock => test_clock,
-			reset => test_reset,
+			reset => '0',
 			source_address => (44 downto 0 => '0') & test_source_address,
 			source_port => test_source_port,
 			destination_address => (44 downto 0 => '0') & test_destination_address,
@@ -60,41 +59,72 @@ architecture address_table_hardware_test_rtl of address_table_hardware_test is
 			output_ready => internal_output_ready
 		);
 		
-	process(test_state, test_clock, test_reset, test_push_pin, internal_output_ready, internal_destination_port)
+	process(test_clock, test_reset, test_state_next)
+	begin
+		if (test_clock'event and test_clock = '0') then
+			test_state <= test_state_next;
+		end if;
+	end process;
+	
+	process (test_clock, test_state, test_push_pin)
 	begin
 		case test_state is
-			when base_state =>
-				if (test_push_pin'event and test_push_pin ='0') then
-					test_state <= push_pin_state;
-					internal_trigger <= '0';
+			when reset_state =>
+				-- if PP is pushed, set to seen_state
+				if (test_push_pin = '0') then
+					test_state_next <= seen_state;
+				-- if PP is not pushed, set to reset_state
+				else
+					test_state_next <= reset_state;
 				end if;
-			when push_pin_state =>
-				if (test_clock'event and test_clock = '0') then
-					test_state <= assert_trigger_state;
-					internal_trigger <= '0';
-				end if;
-			when assert_trigger_state =>
-				if (test_clock'event and test_clock = '0') then
-					test_state <= deassert_trigger_state;
-					internal_trigger <= '1';
-				end if;
-			when deassert_trigger_state =>
-				if (test_clock'event and test_clock = '1' and internal_output_ready = '1') then
-					test_state <= latch_state;
-					internal_trigger <= '0';
-				end if;
-			when latch_state =>
-				if (test_clock'event and test_clock = '1') then
-					test_state <= base_state;
-					internal_trigger <= '0';
-					test_destination_port <= internal_destination_port;
+			when seen_state =>
+				-- always move on
+				test_state_next <= final_state;
+			when final_state =>
+				-- if PP is pushed, keep at final_state
+				if (test_push_pin = '0') then
+					test_state_next <= final_state;
+				-- if PP is not pushed, we can reset
+				else
+					test_state_next <= reset_state;
 				end if;
 		end case;
 	end process;
 	
-	process (internal_destination_port)
+	process(test_state)
 	begin
-		test_not_found <= '1';
+		case test_state is
+			when reset_state =>
+				internal_trigger <= '0';
+			when seen_state =>
+				internal_trigger <= '1';
+			when final_state =>
+				internal_trigger <= '0';
+		end case;
+	end process;
+	
+	--process(test_state)
+	--begin
+	--	case test_state is
+	--		when reset_state =>
+	--			test_destination_port <= "1000";
+	--		when seen_state =>
+	--			test_destination_port <= "0100";
+	--		when final_state =>
+	--			test_destination_port <= "0010";
+	--	end case;
+	--end process;
+		
+	process (test_clock, internal_output_ready, internal_trigger, test_state_next)
+	begin
+		--test_not_found <= internal_output_ready;
+		--if (test_clock'event and test_clock = '1' and internal_output_ready = '1') then
+		if (internal_output_ready'event and internal_output_ready = '1') then
+			-- latch output
+			test_destination_port <= internal_destination_port;
+			--test_not_found <= internal_trigger;
+			test_not_found <= internal_destination_port(3) and internal_destination_port(2) and internal_destination_port(1) and internal_destination_port(0);
+		end if;
 	end process;
 	
 end address_table_hardware_test_rtl;
